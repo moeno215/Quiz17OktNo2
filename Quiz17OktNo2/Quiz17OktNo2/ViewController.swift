001c034eded6199ee8701d4bb62e48515f17c9dc
『　』 <pachembs501@gmail.com>
2017-10-17 10:53:49 +0700

    this is fruit

---
+++ Quiz17OktNo2/Quiz17OktNo2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Quiz17OktNo2
+//
+//  Created by Maulana Hasbi on 10/17/17.
+//  Copyright © 2017 SMK IDN. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
